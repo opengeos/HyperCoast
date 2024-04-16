6eec8e3005102090646d8ab3b910b78d1ff523dc
Qiusheng Wu <giswqs@gmail.com>
2024-04-16 16:05:29 -0400

    Add Map class

--- hypercoast/hypercoast.py
+++ hypercoast/hypercoast.py
@@ -1,1 +1,16 @@
 """Main module."""
+
+import leafmap
+
+
+class Map(leafmap.Map):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def search_emit(self, default_datset="EMITL2ARFL"):
+        """Search for Earth Engine datasets."""
+        self.add("nasa_earth_data", default_dataset=default_datset)
+
+    def search_pace(self, default_datset="PACE_OCI_L2_AOP_NRT"):
+        """Search for Earth Engine datasets."""
+        self.add("nasa_earth_data", default_dataset=default_datset)
