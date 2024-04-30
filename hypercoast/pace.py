916f74593e85ca6314060680762e6cab4cb6d223
Qiusheng Wu <giswqs@gmail.com>
2024-04-30 11:22:51 -0400

    Add docstrings

--- hypercoast/pace.py
+++ hypercoast/pace.py
@@ -1,8 +1,22 @@
+"""This module contains functions to read and process PACE data.
+"""
+
 import xarray as xr
 
 
 def read_pace(filepath, wavelengths=None, method="nearest", **kwargs):
+    """
+    Reads PACE data from a given file and returns an xarray Dataset.
 
+    Args:
+        filepath (str): Path to the file to read.
+        wavelengths (array-like, optional): Specific wavelengths to select. If None, all wavelengths are selected.
+        method (str, optional): Method to use for selection when wavelengths is not None. Defaults to "nearest".
+        **kwargs: Additional keyword arguments to pass to the `sel` method when wavelengths is not None.
+
+    Returns:
+        xr.Dataset: An xarray Dataset containing the PACE data.
+    """
     ds = xr.open_dataset(filepath, group="geophysical_data")
     ds = ds.swap_dims(
         {
