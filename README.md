7dcec8088afba05548265fff905605926202281f
Qiusheng Wu <giswqs@gmail.com>
2024-06-06 21:46:15 -0400

    Add support for NEON AOP data

--- README.md
+++ README.md
@@ -14,7 +14,7 @@
 
 ## Features
 
--   Interactive visualization and analysis of hyperspectral data (e.g., [EMIT](https://earth.jpl.nasa.gov/emit), [PACE](https://pace.gsfc.nasa.gov), [DESIS](https://www.earthdata.nasa.gov/s3fs-public/imported/DESIS_TCloud_Mar0421.pdf))
+-   Interactive visualization and analysis of hyperspectral data, such as [EMIT](https://earth.jpl.nasa.gov/emit), [PACE](https://pace.gsfc.nasa.gov), [DESIS](https://www.earthdata.nasa.gov/s3fs-public/imported/DESIS_TCloud_Mar0421.pdf), [NEON AOP](https://data.neonscience.org/data-products/DP3.30006.001)
 -   Interactive visualization of NASA [ECOSTRESS](https://ecostress.jpl.nasa.gov) data
 -   Interactive extraction and visualization of spectral signatures
 -   Saving spectral signatures as CSV files
--- hypercoast/hypercoast.py
+++ hypercoast/hypercoast.py
@@ -7,13 +7,15 @@ import numpy as np
 from .common import *
 from .desis import *
 from .emit import *
+from .neon import *
 from .pace import *
 from .ui import SpectralWidget
 
 
 class Map(leafmap.Map):
     """
-    A class that extends leafmap.Map to provide additional functionality for hypercoast.
+    A class that extends leafmap.Map to provide additional functionality for
+        hypercoast.
 
     Attributes:
         Any attributes inherited from leafmap.Map.
@@ -27,7 +29,8 @@ class Map(leafmap.Map):
         Initializes a new instance of the Map class.
 
         Args:
-            **kwargs: Arbitrary keyword arguments that are passed to the parent class's constructor.
+            **kwargs: Arbitrary keyword arguments that are passed to the parent
+                class's constructor.
         """
         super().__init__(**kwargs)
 
@@ -35,7 +38,8 @@ class Map(leafmap.Map):
         """Add a layer to the map.
 
         Args:
-            **kwargs: Arbitrary keyword arguments that are passed to the parent class's add_layer method.
+            **kwargs: Arbitrary keyword arguments that are passed to the parent
+                class's add_layer method.
         """
 
         if isinstance(obj, str):
@@ -51,28 +55,34 @@ class Map(leafmap.Map):
 
     def search_emit(self, default_dataset="EMITL2ARFL"):
         """
-        Adds a NASA Earth Data search tool to the map with a default dataset for EMIT.
+        Adds a NASA Earth Data search tool to the map with a default dataset for
+            EMIT.
 
         Args:
-            default_dataset (str, optional): The default dataset to search for. Defaults to "EMITL2ARFL".
+            default_dataset (str, optional): The default dataset to search for.
+                Defaults to "EMITL2ARFL".
         """
         self.add("nasa_earth_data", default_dataset=default_dataset)
 
     def search_pace(self, default_dataset="PACE_OCI_L2_AOP_NRT"):
         """
-        Adds a NASA Earth Data search tool to the map with a default dataset for PACE.
+        Adds a NASA Earth Data search tool to the map with a default dataset for
+            PACE.
 
         Args:
-            default_dataset (str, optional): The default dataset to search for. Defaults to "PACE_OCI_L2_AOP_NRT".
+            default_dataset (str, optional): The default dataset to search for.
+                Defaults to "PACE_OCI_L2_AOP_NRT".
         """
         self.add("nasa_earth_data", default_dataset=default_dataset)
 
     def search_ecostress(self, default_dataset="ECO_L2T_LSTE"):
         """
-        Adds a NASA Earth Data search tool to the map with a default dataset for ECOSTRESS.
+        Adds a NASA Earth Data search tool to the map with a default dataset for
+            ECOSTRESS.
 
         Args:
-            default_dataset (str, optional): The default dataset to search for. Defaults to "ECO_L2T_LSTE".
+            default_dataset (str, optional): The default dataset to search for.
+                Defaults to "ECO_L2T_LSTE".
         """
         self.add("nasa_earth_data", default_dataset=default_dataset)
 
@@ -92,25 +102,40 @@ class Map(leafmap.Map):
         **kwargs,
     ):
         """Add a local raster dataset to the map.
-            If you are using this function in JupyterHub on a remote server (e.g., Binder, Microsoft Planetary Computer) and
-            if the raster does not render properly, try installing jupyter-server-proxy using `pip install jupyter-server-proxy`,
-            then running the following code before calling this function. For more info, see https://bit.ly/3JbmF93.
+            If you are using this function in JupyterHub on a remote server
+                (e.g., Binder, Microsoft Planetary Computer) and
+            if the raster does not render properly, try installing
+                jupyter-server-proxy using `pip install jupyter-server-proxy`,
+            then running the following code before calling this function. For
+                more info, see https://bit.ly/3JbmF93.
 
             import os
             os.environ['LOCALTILESERVER_CLIENT_PREFIX'] = 'proxy/{port}'
 
         Args:
-            source (str): The path to the GeoTIFF file or the URL of the Cloud Optimized GeoTIFF.
-            indexes (int, optional): The band(s) to use. Band indexing starts at 1. Defaults to None.
-            colormap (str, optional): The name of the colormap from `matplotlib` to use when plotting a single band. See https://matplotlib.org/stable/gallery/color/colormap_reference.html. Default is greyscale.
-            vmin (float, optional): The minimum value to use when colormapping the palette when plotting a single band. Defaults to None.
-            vmax (float, optional): The maximum value to use when colormapping the palette when plotting a single band. Defaults to None.
-            nodata (float, optional): The value from the band to use to interpret as not valid data. Defaults to None.
-            attribution (str, optional): Attribution for the source raster. This defaults to a message about it being a local file.. Defaults to None.
+            source (str): The path to the GeoTIFF file or the URL of the Cloud
+                Optimized GeoTIFF.
+            indexes (int, optional): The band(s) to use. Band indexing starts
+                at 1. Defaults to None.
+            colormap (str, optional): The name of the colormap from `matplotlib`
+                to use when plotting a single band. See
+                https://matplotlib.org/stable/gallery/color/colormap_reference.html.
+                Default is greyscale.
+            vmin (float, optional): The minimum value to use when colormapping
+                the palette when plotting a single band. Defaults to None.
+            vmax (float, optional): The maximum value to use when colormapping
+                the palette when plotting a single band. Defaults to None.
+            nodata (float, optional): The value from the band to use to interpret
+                as not valid data. Defaults to None.
+            attribution (str, optional): Attribution for the source raster. This
+                defaults to a message about it being a local file.. Defaults to None.
             layer_name (str, optional): The layer name to use. Defaults to 'Raster'.
-            zoom_to_layer (bool, optional): Whether to zoom to the extent of the layer. Defaults to True.
-            visible (bool, optional): Whether the layer is visible. Defaults to True.
-            array_args (dict, optional): Additional arguments to pass to `array_to_memory_file` when reading the raster. Defaults to {}.
+            zoom_to_layer (bool, optional): Whether to zoom to the extent of the
+                layer. Defaults to True.
+            visible (bool, optional): Whether the layer is visible. Defaults to
+                True.
+            array_args (dict, optional): Additional arguments to pass to
+                `array_to_memory_file` when reading the raster. Defaults to {}.
         """
 
         import numpy as np
@@ -148,26 +173,41 @@ class Map(leafmap.Map):
         array_args={},
         **kwargs,
     ):
-        """Add a local raster dataset to the map.
-            If you are using this function in JupyterHub on a remote server (e.g., Binder, Microsoft Planetary Computer) and
-            if the raster does not render properly, try installing jupyter-server-proxy using `pip install jupyter-server-proxy`,
-            then running the following code before calling this function. For more info, see https://bit.ly/3JbmF93.
+        """Add an EMIT dataset to the map.
+            If you are using this function in JupyterHub on a remote server
+                (e.g., Binder, Microsoft Planetary Computer) and
+            if the raster does not render properly, try installing
+                jupyter-server-proxy using `pip install jupyter-server-proxy`,
+            then running the following code before calling this function. For
+                more info, see https://bit.ly/3JbmF93.
 
             import os
             os.environ['LOCALTILESERVER_CLIENT_PREFIX'] = 'proxy/{port}'
 
         Args:
-            source (str): The path to the GeoTIFF file or the URL of the Cloud Optimized GeoTIFF.
-            indexes (int, optional): The band(s) to use. Band indexing starts at 1. Defaults to None.
-            colormap (str, optional): The name of the colormap from `matplotlib` to use when plotting a single band. See https://matplotlib.org/stable/gallery/color/colormap_reference.html. Default is greyscale.
-            vmin (float, optional): The minimum value to use when colormapping the palette when plotting a single band. Defaults to None.
-            vmax (float, optional): The maximum value to use when colormapping the palette when plotting a single band. Defaults to None.
-            nodata (float, optional): The value from the band to use to interpret as not valid data. Defaults to None.
-            attribution (str, optional): Attribution for the source raster. This defaults to a message about it being a local file.. Defaults to None.
+            source (str): The path to the GeoTIFF file or the URL of the Cloud
+                Optimized GeoTIFF.
+            indexes (int, optional): The band(s) to use. Band indexing starts
+                at 1. Defaults to None.
+            colormap (str, optional): The name of the colormap from `matplotlib`
+                to use when plotting a single band.
+                    See https://matplotlib.org/stable/gallery/color/colormap_reference.html.
+                    Default is greyscale.
+            vmin (float, optional): The minimum value to use when colormapping
+                the palette when plotting a single band. Defaults to None.
+            vmax (float, optional): The maximum value to use when colormapping
+                the palette when plotting a single band. Defaults to None.
+            nodata (float, optional): The value from the band to use to
+                interpret as not valid data. Defaults to None.
+            attribution (str, optional): Attribution for the source raster. This
+                defaults to a message about it being a local file.. Defaults to None.
             layer_name (str, optional): The layer name to use. Defaults to 'EMIT'.
-            zoom_to_layer (bool, optional): Whether to zoom to the extent of the layer. Defaults to True.
-            visible (bool, optional): Whether the layer is visible. Defaults to True.
-            array_args (dict, optional): Additional arguments to pass to `array_to_memory_file` when reading the raster. Defaults to {}.
+            zoom_to_layer (bool, optional): Whether to zoom to the extent of the
+                layer. Defaults to True.
+            visible (bool, optional): Whether the layer is visible. Defaults to
+                True.
+            array_args (dict, optional): Additional arguments to pass to
+                `array_to_memory_file` when reading the raster. Defaults to {}.
         """
 
         xds = None
@@ -368,6 +408,88 @@ class Map(leafmap.Map):
 
         self.cog_layer_dict[layer_name]["xds"] = source
         self.cog_layer_dict[layer_name]["hyper"] = "DESIS"
+
+    def add_neon(
+        self,
+        source,
+        wavelengths=None,
+        indexes=None,
+        colormap=None,
+        vmin=0,
+        vmax=0.5,
+        nodata=np.nan,
+        attribution=None,
+        layer_name="NEON",
+        zoom_to_layer=True,
+        visible=True,
+        array_args={},
+        method="nearest",
+        **kwargs,
+    ):
+        """Add an NEON AOP dataset to the map.
+            If you are using this function in JupyterHub on a remote server
+                (e.g., Binder, Microsoft Planetary Computer) and
+            if the raster does not render properly, try installing
+                jupyter-server-proxy using `pip install jupyter-server-proxy`,
+            then running the following code before calling this function. For
+                more info, see https://bit.ly/3JbmF93.
+
+            import os
+            os.environ['LOCALTILESERVER_CLIENT_PREFIX'] = 'proxy/{port}'
+
+        Args:
+            source (str): The path to the NEON AOP HDF5 file.
+            indexes (int, optional): The band(s) to use. Band indexing starts
+                at 1. Defaults to None.
+            colormap (str, optional): The name of the colormap from `matplotlib`
+                to use when plotting a single band. See
+                    https://matplotlib.org/stable/gallery/color/colormap_reference.html.
+                    Default is greyscale.
+            vmin (float, optional): The minimum value to use when colormapping
+                the palette when plotting a single band. Defaults to 0.
+            vmax (float, optional): The maximum value to use when colormapping
+                the palette when plotting a single band. Defaults to 0.5.
+            nodata (float, optional): The value from the band to use to
+                interpret as not valid data. Defaults to np.nan.
+            attribution (str, optional): Attribution for the source raster. This
+                defaults to a message about it being a local file.. Defaults to None.
+            layer_name (str, optional): The layer name to use. Defaults to 'NEON'.
+            zoom_to_layer (bool, optional): Whether to zoom to the extent of the
+                layer. Defaults to True.
+            visible (bool, optional): Whether the layer is visible. Defaults
+                to True.
+            array_args (dict, optional): Additional arguments to pass to
+                `array_to_memory_file` when reading the raster. Defaults to {}.
+            method (str, optional): The method to use for data interpolation.
+                Defaults to "nearest".
+        """
+
+        xds = None
+        if isinstance(source, str):
+
+            xds = read_neon(source)
+            source = neon_to_image(xds, wavelengths=wavelengths, method=method)
+        elif isinstance(source, xr.Dataset):
+            xds = source
+            source = neon_to_image(xds, wavelengths=wavelengths, method=method)
+
+        self.add_raster(
+            source,
+            indexes=indexes,
+            colormap=colormap,
+            vmin=vmin,
+            vmax=vmax,
+            nodata=nodata,
+            attribution=attribution,
+            layer_name=layer_name,
+            zoom_to_layer=zoom_to_layer,
+            visible=visible,
+            array_args=array_args,
+            **kwargs,
+        )
+
+        self.cog_layer_dict[layer_name]["xds"] = xds
+        self.cog_layer_dict[layer_name]["hyper"] = "NEON"
 
     def set_plot_options(
         self,
