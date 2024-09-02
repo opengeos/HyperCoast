a2cd637222e9a210c48d14079f0fec3406b7e448
Qiusheng Wu <giswqs@gmail.com>
2024-09-02 00:39:50 -0400

    Add PACE workshop notebook (#135)
    * Add PACE workshop notebook
    
    * Update PACE notebooks
    
    * Add hypoxia notebook
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add openpyxl
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add fiona and geopandas
    
    * Update PACE notebook
    
    * Update EARTHDATA secrets
    
    * Update NASA EARTHDATA API Key
    
    * Check file path
    
    * Add earthaccess login test
    
    * Use earthaccess.login
    
    * Fix os import error
    
    * Add earthaccess notebook
    
    * Add env variables
    
    * Update env variable
    
    * Update notebook
    
    * Update PACE notebook
    
    * Update PACE notebook
    
    * Deprecate Python 3.8
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- hypercoast/common.py
+++ hypercoast/common.py
@@ -471,8 +471,14 @@ def nasa_earth_login(strategy: str = "all", persist: bool = True, **kwargs) -> N
         strategy (str, optional): The login strategy. Defaults to "all".
         persist (bool, optional): Whether to persist the login. Defaults to True.
     """
+    from leafmap import get_api_key
+    import earthaccess
 
-    leafmap.nasa_data_login(strategy=strategy, persist=persist, **kwargs)
+    USERNAME = get_api_key("EARTHDATA_USERNAME")
+    PASSWORD = get_api_key("EARTHDATA_PASSWORD")
+    if (USERNAME is not None) and (PASSWORD is not None):
+        strategy = "environment"
+    earthaccess.login(strategy=strategy, persist=persist, **kwargs)
 
 
 def convert_coords(
